a41b6e8dac83a7e8fb703b732ebbd104979eddb8
Christian <cfontanez21@yahoo.com>
2021-03-13 14:19:41 -0500

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,12 +10,24 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    public Text gameoverText;
+    public Text livesText;
+    private int lives = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        score.text = "Score: " + scoreValue.ToString();
+        livesText.text = "Lives: " + lives.ToString();
+        gameoverText.text = "";
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
     }
 
     // Update is called once per frame
@@ -31,6 +43,26 @@ public class PlayerScript : MonoBehaviour
             Application.Quit();
         }
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,8 +70,35 @@ public class PlayerScript : MonoBehaviour
         if(collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            
+            if(scoreValue == 4)
+            {
+                transform.position = new Vector2(68.0f, 0.2f);
+                lives = 3;
+                livesText.text = "Lives: " + lives.ToString();
+            }
+
+            if(scoreValue >= 8)
+            {
+                gameoverText.text = "Good Job! You Won! Game created by Christian Fontanez";
+                musicSource.clip = musicClipTwo;
+                musicSource.Play();
+            }
+        }
+
+        if(collision.collider.tag == "Enemy")
+        {
+            lives -= 1;
+            livesText.text = "Lives: " + lives.ToString();
+            Destroy(collision.collider.gameObject);
+
+            if(lives <= 0)
+            {
+                gameoverText.text = "You have run out of lives!";
+                speed = 0;
+            }
         }
     }
 
